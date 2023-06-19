f73ff890ac922877702df2526562b8ddb75e666f
msperryucsd <msperry@ucsd.edu>
2023-06-19 12:26:42 -0700

    relocate examples

--- examples/runs/delete_stats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Specify the path to the 'graphs' directory
-graphs_dir="graphs"
-
-# Find and delete all 'stats.pickle' files in subdirectories of 'graphs'
-find "$graphs_dir" -type f -name 'stats.pickle' -delete
